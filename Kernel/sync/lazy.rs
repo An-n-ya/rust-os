d3a4805b6b12766ce5602e0ce73b7dfd6a1be432
Annya <annya.host@gmail.com>
2024-06-14 06:13:33 +0800

    add lazy

---
+++ Kernel/sync/lazy.rs
@@ -0,0 +1,41 @@
+use core::{
+    cell::{Cell, OnceCell},
+    ops::Deref,
+};
+
+pub struct Lazy<T, F = fn() -> T> {
+    cell: OnceCell<T>,
+    init: Cell<Option<F>>,
+}
+
+impl<T, F> Lazy<T, F> {
+    pub const fn new(init: F) -> Lazy<T, F> {
+        Lazy {
+            cell: OnceCell::new(),
+            init: Cell::new(Some(init)),
+        }
+    }
+}
+
+impl<T, F: FnOnce() -> T> Lazy<T, F> {
+    pub fn force(this: &Lazy<T, F>) -> &T {
+        this.cell.get_or_init(|| match this.init.take() {
+            Some(f) => f(),
+            None => panic!("Lazy instance has previously been poisoned"),
+        })
+    }
+}
+
+impl<T, F: FnOnce() -> T> Deref for Lazy<T, F> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        Lazy::force(self)
+    }
+}
+
+impl<T: Default> Default for Lazy<T> {
+    fn default() -> Self {
+        Lazy::new(T::default)
+    }
+}
--- Kernel/sync/mod.rs
+++ Kernel/sync/mod.rs
@@ -1,2 +1,3 @@
+pub mod lazy;
 pub mod once;
 pub mod spin;
